5913a5930fae1c19688fc71e3502386c3b843cd7
Nguyễn Duy Hòa <hoa.nguyen2020@hcmut.edu.vn>
2022-12-15 23:41:23 +0700

    nothing

--- components/assignTask/User.js
+++ components/assignTask/User.js
@@ -2,41 +2,182 @@ import React from "react";
 
 const User = () => {
   return (
-    <div>
-      <div class="bg-grey-lighter flex-1 overflow-auto">
-        <div class="px-3 flex items-center bg-grey-light cursor-pointer">
-          <div>
-            <img
+    <div class="rounded border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 w-3/4" >
+      <h1
+        style={{ marginLeft: 1 + "em" }}
+        className="font-semibold text-3xl text-start text-gray-500 my-5"
+      >
+        Janitors
+      </h1>
+      <br></br>
+      <br></br>
+      <div class="grid grid-rows-3 grid-flow-col gap-4">
+        <div class="row-start-1 row-span-2 rounded ">
+          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            
+            <div class="p-5">
+              <a href="#">
+              <img
               class="h-12 w-12 rounded-full"
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png"
-            />
-          </div>
-          <div class="ml-4 flex-1 border-b border-grey-lighter py-4">
-            <div class="flex items-bottom justify-between">
-              <p class="text-grey-darkest">Collector A</p>
-              <p class="text-xs text-grey-darkest">12:52</p>
+              />
+              </a>
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Lionel Scaloni
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                1 tasks
+              </p>
+              <a
+                href="#"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Assign
+                <svg
+                  aria-hidden="true"
+                  class="w-4 h-4 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </a>
             </div>
-            <p class="text-grey-dark mt-1 text-sm">Message Content</p>
           </div>
         </div>
-        <div class="bg-white px-3 flex items-center hover:bg-grey-lighter cursor-pointer">
-          <div>
-            <img
+        <div class="row-start-1 row-span-2 rounded ">
+          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            
+            <div class="p-5">
+              <a href="#">
+              <img
               class="h-12 w-12 rounded-full"
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png"
-            />
-          </div>
-          <div class="ml-4 flex-1 border-b border-grey-lighter py-4">
-            <div class="flex items-bottom justify-between">
-              <p class="text-grey-darkest">Janitor B</p>
-              <p class="text-xs text-grey-darkest">12:30</p>
+              />
+              </a>
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Lionel Scaloni
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                1 tasks
+              </p>
+              <a
+                href="#"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Assign
+                <svg
+                  aria-hidden="true"
+                  class="w-4 h-4 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </a>
             </div>
-            <p class="text-grey-dark mt-1 text-sm">Message Content</p>
+          </div>
+        </div>
+        <div class="row-start-1 row-span-2 rounded ">
+          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            
+            <div class="p-5">
+              <a href="#">
+              <img
+              class="h-12 w-12 rounded-full"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png"
+              />
+              </a>
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Lionel Scaloni
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                1 tasks
+              </p>
+              <a
+                href="#"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Assign
+                <svg
+                  aria-hidden="true"
+                  class="w-4 h-4 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      <div class="row-start-1 row-span-2 rounded ">
+          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            
+            <div class="p-5">
+              <a href="#">
+              <img
+              class="h-12 w-12 rounded-full"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png"
+              />
+              </a>
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Lionel Scaloni
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                1 tasks
+              </p>
+              <a
+                href="#"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Assign
+                <svg
+                  aria-hidden="true"
+                  class="w-4 h-4 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </div>
+      <br></br>
+      <br></br>
     </div>
   );
 };
 
 export default User;
+
